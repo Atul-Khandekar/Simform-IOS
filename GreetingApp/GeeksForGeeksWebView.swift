227a671092c1f1618fcfad0b4d33cfb68374d769
Atul khandekar <atul.k@simformsolutions.com>
2023-05-22 18:15:23 +0530

    Add map view with picker , web view with activity indicator

---
+++ GreetingApp/GeeksForGeeksWebView.swift
@@ -0,0 +1,58 @@
+//
+//  GeeksForGeeksWebView.swift
+//  GreetingApp
+//
+//  Created by Atul Khandekar on 12/05/23.
+//
+
+import UIKit
+import WebKit
+
+class GeeksForGeeksWebView: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak private var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak private var webView: WKWebView!
+    
+    //MARK: - View LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+}
+
+//MARK: - setupView
+extension GeeksForGeeksWebView: WKNavigationDelegate {
+    func setupView() {
+        webView.navigationDelegate = self
+        if let url = URL(string: URL.GeeksForGeeks) {
+            webView.load(URLRequest(url: url))
+        }
+        webView.allowsBackForwardNavigationGestures = true
+    }
+}
+
+//MARK: - UIWebViewDelegate
+extension GeeksForGeeksWebView: UIWebViewDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        loadingIndicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadingIndicator.stopAnimating()
+    }
+}
+
+//MARK: - Actions
+extension GeeksForGeeksWebView {
+    @IBAction private func btnForward(_ sender: UIBarButtonItem) {
+        webView.goForward()
+    }
+    @IBAction private func btnBack(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
---
+++ GreetingApp/URLs.swift
@@ -0,0 +1,12 @@
+//
+//  URLs.swift
+//  GreetingApp
+//
+//  Created by Atul Khandekar on 25/05/23.
+//
+
+import Foundation
+
+extension URL {
+    static let GeeksForGeeks = "https://practice.geeksforgeeks.org/"
+}
